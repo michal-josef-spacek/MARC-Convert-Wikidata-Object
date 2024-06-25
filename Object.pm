707d659c05f41f380614c7674128da7cf2798f77
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-06-25 08:37:30 +0200

    Update copyright years

--- Object.pm
+++ Object.pm
@@ -824,7 +824,7 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
-© Michal Josef Špaček 2021-2023
+© Michal Josef Špaček 2021-2024
 
 BSD 2-Clause License
 
--- Object/Kramerius.pm
+++ Object/Kramerius.pm
@@ -159,7 +159,7 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
-© Michal Josef Špaček 2021-2023
+© Michal Josef Špaček 2021-2024
 
 BSD 2-Clause License
 
