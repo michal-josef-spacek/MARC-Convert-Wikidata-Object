e9fb12911cc153e4480cafc218beb93506a6b316
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-03-17 18:19:37 +0100

    Prepare for 0.11 version

--- Object/Publisher.pm
+++ Object/Publisher.pm
@@ -6,7 +6,7 @@ use warnings;
 use Mo qw(build is);
 use Mo::utils qw(check_required);
 
-our $VERSION = 0.10;
+our $VERSION = 0.11;
 
 has id => (
 	is => 'ro',
@@ -177,6 +177,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.10
+0.11
 
 =cut
